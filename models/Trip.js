56a0d551e18386e076e7f839fb699bd0cc418a34
Tai Mujarin <tai.mujarin@gmail.com>
2021-06-28 11:45:01 -0700

    added trip model

---
+++ models/Trip.js
@@ -0,0 +1,74 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Trip extends Model {}
+
+Trip.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    budget: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    total_travelers: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    restaurant_rec: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    culture: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    sightseeing: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    active_activities: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    relaxing_activities: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    shopping: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    allow_pets: {
+        type: DataTypes.BOOLEAN,
+    },
+    location_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'location',
+        key: 'id',
+        },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Trip',
+  }
+);
+
+module.exports = Trip;
