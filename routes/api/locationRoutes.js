d122b3fb8c6ca38bbd64668b34664dc96d49c796
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-06-28 13:23:00 -0700

    Added a get route to get all locations

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -1,6 +1,18 @@
+////////////////////////////
+// ROUTES for /api/location
+////////////////////////////
+
 const router = require('express').Router();
+const { Location, User, Trip } = require('../../models');
 
-
-
+// GET all locations
+router.get('/', async (req, res) => {
+    try {
+        const locationData = await Location.findAll();
+        res.status(200).json(locationData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
