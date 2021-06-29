ca3064a0850a7efb4dad79425ae41c3032a322d6
Tai Mujarin <tai.mujarin@gmail.com>
2021-06-29 14:45:04 -0700

    fixed .get for login and share

--- routes/api/shareRoutes.js
+++ routes/api/shareRoutes.js
@@ -2,6 +2,14 @@ const router = require('express').Router();
 const {Location, Photos, Trip, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    res.render('share');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post('/share', withAuth, async (req, res) => {
     try {
       const shareData = await Share.create(req.body, {
