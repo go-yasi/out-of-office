40dd3902dbe667bf7b2841cf830c856ff94b8028
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-06-28 17:10:23 -0700

    Changed login routes to user routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const locationRoutes = require('./locationRoutes');
-const loginRoutes = require('./loginRoutes');
+const userRoutes = require('./userRoutes');
 const shareRoutes = require('./shareRoutes');
 const tripRoutes = require('./tripRoutes');
 
 router.use('/location', locationRoutes);
-router.use('/login', loginRoutes);
+router.use('/users', userRoutes);
 router.use('/share', shareRoutes);
 router.use('/trip', tripRoutes);
 
